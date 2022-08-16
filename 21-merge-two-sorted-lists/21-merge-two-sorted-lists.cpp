9c9475a095283929855a24a41bcf61f502fa3345
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-08-16 13:36:30 +0530

    Time: 7 ms (87.88%), Space: 14.8 MB (81.47%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,33 +11,53 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-    ListNode* p1 = list1;
-    ListNode* p2 = list2;
-    ListNode* dummynode = new ListNode(-1);
-    ListNode* p3 = dummynode;
+//     ListNode* p1 = list1;
+//     ListNode* p2 = list2;
+//     ListNode* dummynode = new ListNode(-1);
+//     ListNode* p3 = dummynode;
     
-    while(p1!=NULL && p2!=NULL){
-        if(p1->val < p2->val){
-            p3->next = p1;
-            p1 = p1->next;
-        }
-        else{
-            p3->next = p2;
-            p2 = p2->next;
-        }
-        p3 = p3->next;
-    }
-    while(p1!=NULL){
-        p3->next = p1;
-        p1 = p1->next;
-        p3 = p3->next;
+//     while(p1!=NULL && p2!=NULL){
+//         if(p1->val < p2->val){
+//             p3->next = p1;
+//             p1 = p1->next;
+//         }
+//         else{
+//             p3->next = p2;
+//             p2 = p2->next;
+//         }
+//         p3 = p3->next;
+//     }
+//     while(p1!=NULL){
+//         p3->next = p1;
+//         p1 = p1->next;
+//         p3 = p3->next;
+//     }
+    
+//     while(p2!=NULL){
+//         p3->next = p2;
+//         p2 = p2->next;
+//         p3 = p3->next;
+//     }
+//     return dummynode->next;
+    
+        //recursive way
+    if(list1==NULL){
+        return list2;
     }
     
-    while(p2!=NULL){
-        p3->next = p2;
-        p2 = p2->next;
-        p3 = p3->next;
+    if(list2==NULL){
+        return list1;
     }
-    return dummynode->next;
+    
+    ListNode* result;
+    if(list1->val < list2->val){
+        result = list1;
+        result->next = mergeTwoLists(list1->next , list2);
+    }
+    else{
+        result = list2;
+        result->next = mergeTwoLists(list1 , list2->next);
+    }
+    return result;
     }
 };
