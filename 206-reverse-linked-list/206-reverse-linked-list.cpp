a7a98099412c3edf4b607d44b031d4e3a27e50cc
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-08-17 14:16:02 +0530

    Time: 7 ms (80.82%), Space: 8.6 MB (10.31%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -8,35 +8,36 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Pair{
-public:
-    ListNode* head;
-    ListNode* tail;
-};
+// class Pair{
+// public:
+//     ListNode* head;
+//     ListNode* tail;
+// };
 
-Pair reverse_LL_optimised(ListNode* head){
-    if(head == NULL || head->next == NULL){
-        Pair ans;
-        ans.head = head;
-        ans.tail = head;
-        return ans;
-    }
-Pair smallans = reverse_LL_optimised(head->next);
-    smallans.tail->next = head;
-    head->next = NULL;
+// Pair reverse_LL_optimised(ListNode* head){
+//     if(head == NULL || head->next == NULL){
+//         Pair ans;
+//         ans.head = head;
+//         ans.tail = head;
+//         return ans;
+//     }
+// Pair smallans = reverse_LL_optimised(head->next);
+//     smallans.tail->next = head;
+//     head->next = NULL;
     
-Pair ans;
-    ans.head = smallans.head;
-    ans.tail = head;
+// Pair ans;
+//     ans.head = smallans.head;
+//     ans.tail = head;
     
-    return ans;
-}
+//     return ans;
+// }
 
-class Solution {
-public:
-    ListNode* reverseList(ListNode* head) {
-    return reverse_LL_optimised(head).head;
-}
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//     return reverse_LL_optimised(head).head;
+//     }
+// };
         // this code has complexity O(n2)
         // base case
 //         if(head == NULL || head->next == NULL){
@@ -55,4 +56,18 @@ public:
 //         head->next = NULL;
         
 //         return small_ans;
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if(head == NULL || head->next == NULL){
+            return head;
+        } 
+    
+        ListNode* smallans = reverseList(head->next);
+        ListNode* tail = head->next;
+        tail->next = head;
+        head->next = NULL;
+        return smallans;
+    }
 };
