a9bed9b40860f98df4611493c9719d3b19cc9270
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-08-17 13:04:19 +0530

    Time: 6 ms (85.08%), Space: 8.6 MB (10.31%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -8,25 +8,51 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class Pair{
+public:
+    ListNode* head;
+    ListNode* tail;
+};
+
+Pair reverse_LL_optimised(ListNode* head){
+    if(head == NULL || head->next == NULL){
+        Pair ans;
+        ans.head = head;
+        ans.tail = head;
+        return ans;
+    }
+Pair smallans = reverse_LL_optimised(head->next);
+    smallans.tail->next = head;
+    head->next = NULL;
+    
+Pair ans;
+    ans.head = smallans.head;
+    ans.tail = head;
+    
+    return ans;
+}
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+    return reverse_LL_optimised(head).head;
+}
+        // this code has complexity O(n2)
         // base case
-        if(head == NULL || head->next == NULL){
-            return head;    
-        }
+//         if(head == NULL || head->next == NULL){
+//             return head;    
+//         }
         
-        // recursive condition
-        ListNode* small_ans = reverseList(head->next);
+//         // recursive condition
+//         ListNode* small_ans = reverseList(head->next);
         
-        // small calc
-        ListNode* temp = small_ans;
-        while(temp->next != NULL){
-            temp = temp->next;
-        }
-        temp->next = head;
-        head->next = NULL;
+//         // small calc
+//         ListNode* temp = small_ans;
+//         while(temp->next != NULL){
+//             temp = temp->next;
+//         }
+//         temp->next = head;
+//         head->next = NULL;
         
-        return small_ans;
-    }
+//         return small_ans;
 };
