a4e0d56ef7c39b7a11666a94fd54a0da0e22788b
UTKARSH GUPTA <72146426+utkarsh123871@users.noreply.github.com>
2022-10-21 16:16:17 +0530

    Time: 28 ms (24.43%), Space: 6 MB (38.03%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int fibonacci_helper(int n , int *ans){
+    // base case
+    if(n<=1){
+        return n;
+    }
+    
+    // check if output already exists
+    if(ans[n] != -1){
+        return ans[n];    
+    }
+    
+    int a = fibonacci_helper(n-1 , ans);
+    int b = fibonacci_helper(n-2 , ans);
+    
+    // save the output for future
+    ans[n] = a+b;
+    
+    // returning the final output
+    return ans[n];
+}
+    
+    int fib(int n) {
+    // int *ans = new int[n+1];
+    // for (int i = 0; i <= n; i++) {
+    //     ans[i]= -1;
+    //     }
+    // return fibonacci_helper(n , ans);
+        if(n == 0)
+            return 0;
+        
+        if(n == 1)
+            return 1;
+    
+        int ans = fib(n-1) + fib(n-2);
+ 
+        return ans;
+    }
+};
